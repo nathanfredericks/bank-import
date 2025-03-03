889b679f8389124364e3b29f7b29cb286e9ebcb3
Nathan Fredericks <nathan@fredericks.dev>
2025-03-03 01:30:18 -0400

    Fix proxy

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,18 +1,4 @@
-import axios, { CreateAxiosDefaults } from "axios";
-import env from "./env.js";
+import axios from "axios";
 
-const config: CreateAxiosDefaults = {};
-if (env.PROXY_SERVER && env.PROXY_USERNAME && env.PROXY_PASSWORD) {
-  const url = new URL(env.PROXY_SERVER);
-  config.proxy = {
-    protocol: url.protocol,
-    host: url.host.split(":")[0],
-    port: parseInt(url.port),
-    auth: {
-      username: env.PROXY_USERNAME,
-      password: env.PROXY_PASSWORD,
-    },
-  };
-}
-const instance = axios.create(config);
+const instance = axios.create();
 export default instance;
