36a18e1961b8c5eabae5339a62d074c3198b7bfd
Nathan Fredericks <nathan@fredericks.dev>
2025-12-08 00:14:32 -0400

    Format

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -30,14 +30,14 @@ const Secrets = z.object({
   BMO_PASSWORD: z.string(),
   ROGERS_BANK_USERNAME: z.string(),
   ROGERS_BANK_PASSWORD: z.string(),
+  TANGERINE_USERNAME: z.string(),
+  TANGERINE_PASSWORD: z.string(),
   NBDB_USER_ID: z.string(),
   NBDB_PASSWORD: z.string(),
   PUSHOVER_TOKEN: z.string(),
   PUSHOVER_USER: z.string(),
   YNAB_ACCESS_TOKEN: z.string(),
   JMAP_BEARER_TOKEN: z.string(),
-  TANGERINE_USERNAME: z.string(),
-  TANGERINE_PASSWORD: z.string(),
 });
 
 const secretJson = JSON.parse(SecretString || "{}");
