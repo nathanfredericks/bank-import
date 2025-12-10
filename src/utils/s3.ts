180216366124df19fe620d8e0ca65ac62fcc1556
Nathan Fredericks <nathan@fredericks.dev>
2025-12-09 22:36:27 -0400

    Fix Bun Node API import error

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -6,7 +6,7 @@ import {
   S3Client,
   S3ClientConfig,
 } from "@aws-sdk/client-s3";
-import { writeFile } from "fs/promises";
+import { writeFile } from "node:fs/promises";
 import env from "./env";
 import logger from "./logger";
 
